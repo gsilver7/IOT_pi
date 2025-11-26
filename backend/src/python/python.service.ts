002ea8152ade1b4b93c6e0766fa09299def73219
김명준 <qweasl@naver.com>
2025-11-27 03:09:16 +0900

    feat: 파이썬 가상환경으로 변경

--- backend/src/python/python.service.ts
+++ backend/src/python/python.service.ts
@@ -4,12 +4,12 @@ import * as path from 'path';
 
 @Injectable()
 export class PythonExecutorService {
+
   private readonly scriptPath = path.join(process.cwd(), 'src', 'python', 'face_main.py');
 
   executePythonScript(args?: string[]): Promise<string> {
     return new Promise((resolve, reject) => {
-      const pythonPath = 'python3';
-
+      const pythonPath = '/opt/venv/bin/python';
 
       const scriptArgs = args || [];
       const pythonProcess = spawn(pythonPath, [this.scriptPath, ...scriptArgs]);
