dc98fb7b38d91fa42b28abab7d712b629468ecc0
김명준 <qweasl@naver.com>
2025-12-01 02:42:49 +0900

    fix: api 요청 수정 중

--- backend/src/face/face.controller.ts
+++ backend/src/face/face.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
 import { FaceService } from './face.service';
 import { AuthGuard } from '@nestjs/passport';
-import { EventsGateway } from '../socket/socket.gateway';
+import { StreamGateway } from 'src/stream/stream.gateway';
 
 
 @Controller('face')
 export class FaceController {
   constructor(
     private readonly faceService: FaceService,
-    private readonly eventsGateway: EventsGateway,
+    private readonly eventsGateway: StreamGateway,
   ) {}
 
   @Post('register')
@@ -19,9 +19,10 @@ export class FaceController {
   ) {
     const userId = req.user.id; // JWT에서 유저 ID 추출
 
-    this.eventsGateway.server.emit('python', {
-          scriptPath: '/home/rlaaudwns/web/pi/face_main.py',
-        });
+    this.eventsGateway.server.emit('python', 
+      "python"
+    );
+    console.log("python");
 
     return this.faceService.registerFace(userId, imageData);
   }
