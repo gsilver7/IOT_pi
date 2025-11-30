9fd33c3dfb349821536a0023616e00bbfd5d098a
김명준 <qweasl@naver.com>
2025-12-01 04:46:41 +0900

    feat:의존성 추가

--- backend/src/stream/stream.module.ts
+++ backend/src/stream/stream.module.ts
@@ -2,8 +2,10 @@
 import { Module } from '@nestjs/common';
 import { StreamGateway } from './stream.gateway';
 import { StreamController } from './stream.controller';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports:[UserModule],
   providers: [StreamGateway],
   controllers: [StreamController],
   exports:[StreamGateway],
