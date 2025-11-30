3a8854399488f2d45e6e900e50569502df03135a
gsilver7 <kmjmath002@gmail.com>
2025-11-30 16:59:51 +0900

    chore: https 로 변경

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -16,7 +16,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://kmj.shscript.com/api/auth/login", {
+      const response = await fetch("https://kmj.shscript.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
