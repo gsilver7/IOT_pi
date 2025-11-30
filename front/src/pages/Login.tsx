4a7c448b9afd49b0af3f85a95b9f6f2bc48cf559
gsilver7 <kmjmath002@gmail.com>
2025-11-30 16:58:10 +0900

    chore: 로그인 주소 변경

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -16,7 +16,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:3000/auth/login", {
+      const response = await fetch("http://kmj.shscript.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
