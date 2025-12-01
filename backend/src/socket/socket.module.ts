45a9a8435c77c582be4ee54a0869ebe171a026a3
김명준 <qweasl@naver.com>
2025-12-01 22:35:17 +0900

    fix: 의존성 주입

--- backend/src/socket/socket.module.ts
+++ backend/src/socket/socket.module.ts
@@ -1,8 +1,12 @@
 // events.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsGateway } from './socket.gateway';
+import { User } from '../user/user.entity'; // 경로는 프로젝트 구조에 맞게 수정
+
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [EventsGateway], // 👈 여기에 등록해야 Gateway가 활성화됨
 })
 export class EventsModule {}
